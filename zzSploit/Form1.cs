11a18439a118a606214d4bc9388d9d3f7b01cf31
parks <parks@LAPTOP-L8414OBQ>
2024-10-19 23:34:52 -0400

    Add project files.

---
+++ zzSploit/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cxapi;
+
+
+
+namespace zzSploit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CoreFunctions.Inject();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CoreFunctions.KillRoblox();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CoreFunctions.ExecuteScript(richTextBox1.Text);
+        }
+    }
+}
